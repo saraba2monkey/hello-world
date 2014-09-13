8accb311b2e6c8dfa1cf1368bc23a182c7fde050
saraba_monkey <sarabacoder@163.com>
2014-09-13 21:07:22 +0800

    My first commint

---
+++ MyReadFile.java
@@ -0,0 +1,20 @@
+//Read the file called 'hello.txt' and print the file content
+import java.io.*;
+public class MyReadFile
+{
+	public static void main(String args[]){
+	try{
+		File f = new File("hello.txt");
+		FileInputStream fis = new FileInputStream(f);
+		int length = fis.available();
+		for(int i =0;i<length;i++){
+		System.out.print((char)fis.read()+"#");
+		}
+	}
+	catch(Exception e){
+		/*error:File not found */
+	 System.out.print("File not found.");
+	}
+	
+	}
+}
