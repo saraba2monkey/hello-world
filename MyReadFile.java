166e4443b02b0cd45ddeeaca6af618aaaa0929e4
saraba_monkey <sarabacoder@163.com>
2014-09-13 21:10:55 +0800

    2nd Second commint

--- MyReadFile.java
+++ MyReadFile.java
@@ -10,6 +10,7 @@ public class MyReadFile
 		for(int i =0;i<length;i++){
 		System.out.print((char)fis.read()+"#");
 		}
+		System.out.println("\(^-^\)Hello world!");
 	}
 	catch(Exception e){
 		/*error:File not found */
